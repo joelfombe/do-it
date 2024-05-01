b9346fec60d5673c8d3f5d434dce81b8a6edc657
joelfombe <joeljuniormnauel@gmail.com>
2024-05-01 02:06:52 +0200

    Add new dependencies and update layout in package.json and layout.tsx

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransofrmationsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TransofrmationsPage
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ApdateTransformationPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ApdateTransformationPage
